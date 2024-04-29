4fe039eba33aa716e668b571b78bcd3a0574b266
KannetiDivya <168443589+KannetiDivya@users.noreply.github.com>
2024-04-29 21:54:27 +0530

    Add files via upload

---
+++ scriipt.js
@@ -0,0 +1,100 @@
+let expenses = [];
+let totalAmount = 0;
+
+const categorySelect = document.getElementById('category-select');
+const amountInput = document.getElementById('amount-Input');
+const dateInput = document.getElementById('date-input');
+const addBtn = document.getElementById('add-btn');
+const expensesTableBody = document.getElementById('expensive-table-body');
+const totalAmountCell = document.getElementById('total-amount');
+
+addBtn.addEventListener('click', function() {
+    const category = categorySelect.value;
+    const amount = Number(amountInput.value);
+    const date = dateInput.value;
+
+    if (category === '') {
+        alert('Please select a category');
+        return;
+    }
+    if (isNaN(amount) || amount <=0 ) {
+        alert('Please enter a valid amount')
+        return;
+    }
+    if(date === '') {
+        alert('Please select a date')
+        return;
+    }
+    expenses.push({category, amount, date});
+
+    totalAmount += amount;
+    totalAmountCell.textContent = totalAmount;
+
+    const newRow = expensesTableBody.insertRow();
+
+    const categoryCell = newRow.insertCell();
+    const amountCell = newRow.insertCell();
+    const dateCell = newRow.insertCell();
+    const deleteCell = newRow.insertCell();
+    const editCell = newRow.insertCell();
+    const deleteBtn = document.createElement('button');
+
+    deleteBtn.textContent = 'Delete';
+    deleteBtn.classList.add('delete-btn');
+    deleteBtn.addEventListener('click', function() {
+        if(confirm("Are you sure to delete"))
+        {
+
+        expenses.splice(expenses.indexOf(expense), 1);
+
+        totalAmount -= expense.amount;
+        totalAmountCell.textContent = totalAmount;
+
+        expensesTableBody.removeChild(newRow);
+        }
+    });
+
+    const editBtn = document.createElement('button');
+    editBtn.textContent = 'Edit';
+    editBtn.classList.add('edit-btn');
+
+    const expense = expenses[expenses.length - 1];
+    categoryCell.textContent = expense.category;
+    amountCell.textContent = expense.amount;
+    dateCell.textContent = expense.date;
+    deleteCell.appendChild(deleteBtn);
+    editCell.appendChild(editBtn);
+
+});
+
+for (const expense of expenses) {
+    totalAmount += expense.amount;
+    totalAmountCell.textContent = totalAmount;
+
+    const newRow = expensesTableBody.inserRow();
+    const categoryCell = newRow.insertCell();
+    const amountCell = newRow.insertCell();
+    const dateCell = newRow.insertCell();
+    const deleteCell = newRow.insertCell();
+    const editCell = newRow.insertCell();
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'Delete';
+    deleteBtn.classList.add('delete-btn');
+    deleteBtn.addEventListener('click', function() {
+            
+        expenses.splice(expenses.indexOf(expense), 1);
+
+        totalAmount -= expense.amount;
+        totalAmountCell.textContent = totalAmount;
+        expensesTableBody.removeChild(newRow);
+    });
+    const editBtn = document.createElement('button');
+    editBtn.textContent = 'Edit';
+    editBtn.classList.add('edit-btn');
+
+    categoryCell.textContent = expense.category;
+    amountCell.textContent = expense.amount;
+    dateCell.textContent = expense.date;
+    deleteCell.appendChild(deleteBtn);
+    editCell.appendChild(editBtn);
+}
